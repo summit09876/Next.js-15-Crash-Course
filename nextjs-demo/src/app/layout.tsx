44142c87d788a3a2bf9719c8b8dce92ed04e0909
Vishwas <vishwas.g5@gmail.com>
2024-10-24 11:00:04 +0530

    Modify root layout

--- nextjs-demo/src/app/layout.tsx
+++ nextjs-demo/src/app/layout.tsx
@@ -28,7 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white p-4 text-center">
+          <p>Welcome to Next.js 15 Crash Course</p>
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white p-4 text-center">
+          Codevolution
+        </footer>
       </body>
     </html>
   );
