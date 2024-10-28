7deee20c83aa9a5c37330bc48cc412c31b3b1793
Vishwas <vishwas.g5@gmail.com>
2024-10-28 14:23:53 +0530

    Authentication

--- nextjs-demo/src/app/components/navigation.tsx
+++ nextjs-demo/src/app/components/navigation.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export const Navigation = () => {
   const pathname = usePathname();
   return (
-    <nav>
+    <nav className="flex justify-center items-center p-4">
       <Link
         href="/"
         className={pathname === "/" ? "font-bold mr-4" : "text-blue-500 mr-4"}
@@ -30,6 +31,12 @@ export const Navigation = () => {
       >
         Product 1
       </Link>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </nav>
   );
 };
--- nextjs-demo/src/app/layout.tsx
+++ nextjs-demo/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Navigation } from "./components/navigation";
 
@@ -25,19 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <header className="bg-slate-900 text-white p-4 text-center">
-          {/* <p>Welcome to Next.js 15 Crash Course</p> */}
-          <Navigation />
-        </header>
-        {children}
-        <footer className="bg-slate-900 text-white p-4 text-center">
-          Codevolution
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className="bg-slate-900 text-white p-4 text-center">
+            {/* <p>Welcome to Next.js 15 Crash Course</p> */}
+            <Navigation />
+          </header>
+          {children}
+          <footer className="bg-slate-900 text-white p-4 text-center">
+            Codevolution
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
